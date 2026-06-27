9c4e87e573666bb2a94acff262739c6f09a1fbb2
ChromQuiz <yxvvckzzxr@privaterelay.appleid.com>
2026-06-27 11:30:01 +0900

    Use loopback address for local login

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ function useSupabaseAuth() {
 }
 
 function getLocalhostIndexUrl() {
-    return 'http://localhost:8000/index.html';
+    return 'http://127.0.0.1:8000/index.html';
 }
 
 function setTab(tab) {
